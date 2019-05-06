96f091f560bc28fe34aa123b8ef95da80a9305d0
Mr. Bartucz <jobartucz@isd535.org>
2019-05-06 10:38:13 -0500

    Added a countdown timer and score

--- tkinter_callback_test.py
+++ tkinter_callback_test.py
@@ -1,20 +1,38 @@
 # I'm using a graphical button instead of a real one
 from tkinter import *
+from time import sleep
+from random import uniform
 
-# keep track of whether the game is over
-game_over = False
+# keep track of the score
+player1_wins = 0
+player2_wins = 0
+
+# don't let someone click when they're not supposed to
+can_click = False
+print("Are you ready?") # LED goes on (or make a three-LED countdown)
+print("3....")
+sleep(uniform(1,3))
+print("2....")
+sleep(uniform(1,3))
+print("1....")
+sleep(uniform(1,3))
+print("Go!")            # LED turns off
+can_click = True
 
 def pressed():
     
-    # get the value of "game_over" from the global program - don't make a new one!
-    global game_over
+    # get the values of "can_click" and "wins" from the global program - don't make a new one!
+    global can_click
+    global player1_wins
+    global player2_wins
     
-    if (game_over == False):
-        print ("clicked!")
-        game_over = True
+    if (can_click == True):
+        print ("player 1 wins!")
+        player1_wins += 1
+        can_click = False
     else:
         print ("too slow!")
-        game_over = True
+
 
 # this code just assigns the function about to the button
 b = Button(text="click me", command=pressed)
