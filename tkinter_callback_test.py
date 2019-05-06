3aa0d25e2960f9189606ba38aecd83aada20b285
Mr. Bartucz <jobartucz@isd535.org>
2019-05-06 11:06:46 -0500

    removed score

--- tkinter_callback_test.py
+++ tkinter_callback_test.py
@@ -3,20 +3,13 @@ from tkinter import *
 from time import sleep
 from random import uniform
 
-# keep track of the score
-player1_wins = 0
-player2_wins = 0
-
 def pressed():
     
     # get the values of "can_click" and "wins" from the global program - don't make a new one!
     global can_click
-    global player1_wins
-    global player2_wins
     
     if (can_click == True):
         print ("player 1 wins!")
-        player1_wins += 1
         can_click = False
     else:
         print ("too slow!")
