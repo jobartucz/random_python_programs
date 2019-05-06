05eaba8c6103fb7ee5796538d822ecea63881e83
Mr. Bartucz <jobartucz@isd535.org>
2019-05-06 10:39:08 -0500

    Update tkinter_callback_test.py

--- tkinter_callback_test.py
+++ tkinter_callback_test.py
@@ -7,18 +7,6 @@ from random import uniform
 player1_wins = 0
 player2_wins = 0
 
-# don't let someone click when they're not supposed to
-can_click = False
-print("Are you ready?") # LED goes on (or make a three-LED countdown)
-print("3....")
-sleep(uniform(1,3))
-print("2....")
-sleep(uniform(1,3))
-print("1....")
-sleep(uniform(1,3))
-print("Go!")            # LED turns off
-can_click = True
-
 def pressed():
     
     # get the values of "can_click" and "wins" from the global program - don't make a new one!
@@ -34,6 +22,19 @@ def pressed():
         print ("too slow!")
 
 
+# don't let someone click when they're not supposed to
+can_click = False
+print("Are you ready?") # LED goes on (or make a three-LED countdown)
+print("3....")
+sleep(uniform(1,3))
+print("2....")
+sleep(uniform(1,3))
+print("1....")
+sleep(uniform(1,3))
+print("Go!")            # LED turns off
+can_click = True
+        
+        
 # this code just assigns the function about to the button
 b = Button(text="click me", command=pressed)
 b.pack()
