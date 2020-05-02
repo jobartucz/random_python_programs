09d24fc08630cc20b43107c0f57aeeb0bed2238e
John Bartucz <jbartucz@Johns-Air.roc.mn.charter.com>
2020-05-02 18:49:53 -0500

    added memo

--- painting_tiles.py
+++ painting_tiles.py
@@ -1,16 +1,11 @@
 problem = '''You have a path of n tiles in your garden that you'd like to paint, and four different colors to do so.
-
 'Thing is... You don't want any two adjacent tiles to be of the same color and you'd like that it'd cost you the minimum possible amount of money.
 'Second thing is... due to differences in the texture and matter of each tile, the quantity of paint needed to cover each tile may differe a lot.
-
 So, you are given n x 4 array, that represents the costs of painting each tile of the path with the four available colors and you need to find a way to get the minimal total cost of painting the whole path, without two consecutive tiles being of the same color.
 
 Example
-
 For 6 tiles:
-
 # color:   1  2  3  4
-
 costs = [[ 1, 3, 4, 5],  #  tile 1
            ^
          [ 2, 3, 2, 3],  #   2
@@ -25,11 +20,9 @@ costs = [[ 1, 3, 4, 5],  #  tile 1
               ^
 
 Optimal painting: tile 1 with color 1, tile 2 with color 3, tile 3 with color 2, tile 4 with color 1, tile 5 with color 3, tile 6 with color 2.
-
 Total cost is costs[0][0] + costs[1][2] + costs[2][1] + costs[3][0] + costs[4][2] + costs[5][1] == 1+2+1+2+2+1 == 9.'''
 
-
-def getpathcost(costs, stepnum, laststep):
+def getpathcost_brute(costs, stepnum, laststep):
 
     if stepnum == len(costs):
         return 0
@@ -50,6 +43,27 @@ def getpathcost(costs, stepnum, laststep):
 
     return mincost
 
+def getpathcost_memo(costs, stepnum, laststep, savedcosts):
+
+    if stepnum == len(costs):
+        return 0
+
+    mincost = 9999
+    for c in range(len(costs[stepnum])):
+        if c == laststep:
+            continue
+
+        if savedcosts[stepnum][c] > 0:
+            thiscost = savedcosts[stepnum][c]
+            print("HEY!")
+        else:
+            thiscost = costs[stepnum][c] + getpathcost_memo(costs, stepnum + 1, c, savedcosts)
+            savedcosts[stepnum][c] = thiscost
+
+        if thiscost < mincost:
+            mincost = thiscost
+
+    return mincost
 
 # should == 9
 costinput1 = [[ 1, 3, 4, 5],  #  tile 1
@@ -73,4 +87,8 @@ costinput3 = [[2, 10, 4, 1],
           [4, 2, 7, 10],
           [9, 4, 1, 5]]
 
-print(getpathcost(costinput1, 0, -1))
+
+costinput = costinput3
+savedcosts = [[-1 for x in range(len(costinput[0]) + 1)] for y in range(len(costinput))]
+
+print(getpathcost_memo(costinput, 0, -1, savedcosts))
