2921c93ba6e71304bf3df1acde413950b5e3452a
John Bartucz <jbartucz@Johns-Air.roc.mn.charter.com>
2020-05-03 21:51:33 -0500

    doesn't work

--- next_biggest_num_same_digits.py
+++ next_biggest_num_same_digits.py
@@ -1,4 +1,4 @@
-problem = '''
+problem = '''https://www.codewars.com/kata/55983863da40caa2c900004e/train/python
 Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
 
 12 ==> 21
