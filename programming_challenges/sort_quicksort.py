c7b727d3bb362e259a6c562a3ac5ba11331fb37f
John Bartucz <jbartucz@Johns-Air.roc.mn.charter.com>
2020-05-13 21:27:32 -0500

    qs works!

---
+++ programming_challenges/sort_quicksort.py
@@ -0,0 +1,23 @@
+
+from random import randint
+
+def qs(l):
+
+    if len(l) <= 1:
+        return l
+
+    # we'll always choose the last element as the pivot
+    pivot = l[randint(0, len(l) - 1)]
+
+    a = []
+    b = []
+    for i in l:
+        if i < pivot:
+            a.append(i)
+        else:
+            b.append(i)
+
+    return qs(a) + qs(b)
+
+
+print(qs([5,4,3,2,1,0]))
